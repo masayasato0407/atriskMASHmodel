f685c670d113aa41f21a90fe2f3e4938d05fa3d0
Masaya Sato <masayasato0407@gmail.com>
2025-04-21 14:35:44 +0900

    Update atriskMASH.py

--- atriskMASH.py
+++ atriskMASH.py
@@ -13,10 +13,10 @@ st.markdown("URL of the original article: To be determined")
 
 st.header('Input variables and press the predict button')
 age = st.number_input('Patient age', min_value=18, max_value=100, value=50)
-DM = 1 if st.radio('DM (Diabetes)', ['Absent', 'Present']) == 'Present' else 0
-HTN = 1 if st.radio('HTN (Hypertension)', ['Absent', 'Present']) == 'Present' else 0
-AST = st.number_input('AST', min_value=1, max_value=300, value=30)
-GGT = st.number_input('γ-GTP', min_value=1, max_value=1000, value=30)
+DM = 1 if st.radio('Diabetes', ['Absent', 'Present']) == 'Present' else 0
+HTN = 1 if st.radio('Hypertension', ['Absent', 'Present']) == 'Present' else 0
+AST = st.number_input('AST (U/L)', min_value=1, max_value=300, value=30)
+GGT = st.number_input('γ-GTP (U/L)', min_value=1, max_value=1000, value=30)
 Plt = st.number_input('Platelet count ( × 104/µL)', min_value=1.0, max_value=75.0, value=20.0,step=0.1,format="%.1f")
 INR = st.number_input('PT-INR', min_value=0.50, max_value=3.00, value=1.00, step=0.01, format="%.2f")
 
